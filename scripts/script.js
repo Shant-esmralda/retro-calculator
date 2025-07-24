90118c210d828b6c8d809854d4eac3f524d27808
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-07-24 18:13:09 +0330

    moved around the js a bit. made it shorter. added number 9

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 // BUTTON 7 KEYBOARD BIND
 const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
 // Add a keydown event listener to the document
 document.addEventListener("keydown", function (event) {
   // Check if the pressed key is '7'
@@ -7,27 +9,27 @@ document.addEventListener("keydown", function (event) {
     // Prevent the default action (e.g., submitting a form if the button is inside one)
     // event.preventDefault();
     // Trigger the button's click event
-    btn7.classList.add("nes-btn-primary-keyboard");
     btn7.click();
+    btn7.classList.add("nes-btn-primary-keyboard");
     document.addEventListener("keyup", function (event) {
       btn7.classList.remove("nes-btn-primary-keyboard");
+    });
+  }
+  else if (event.key === "8") {
+    btn8.click();
+    btn8.classList.add("nes-btn-primary-keyboard");
+    document.addEventListener("keyup", function (event) {
+      btn8.classList.remove("nes-btn-primary-keyboard");
+    });
+  }
+    else if (event.key === "9") {
+    btn9.click();
+    btn9.classList.add("nes-btn-primary-keyboard");
+    document.addEventListener("keyup", function (event) {
+      btn9.classList.remove("nes-btn-primary-keyboard");
     });
   }
 });
 
 // BUTTON 8 KEYBOARD BIND
-const btn8 = document.getElementById("btn8");
-// Add a keydown event listener to the document
-document.addEventListener("keydown", function (event) {
-  // Check if the pressed key is '7'
-  if (event.key === "8") {
-    // Prevent the default action (e.g., submitting a form if the button is inside one)
-    // event.preventDefault();
-    // Trigger the button's click event
-    btn8.classList.add("nes-btn-primary-keyboard");
-    btn8.click();
-    document.addEventListener("keyup", function (event) {
-      btn8.classList.remove("nes-btn-primary-keyboard");
-    });
-  }
-});
+

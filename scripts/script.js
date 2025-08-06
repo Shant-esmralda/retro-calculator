f93fd59a1469dbe3d2ed97cdb261e34bb02eb234
Shant <68810456+Shant-esmralda@users.noreply.github.com>
2025-08-06 13:33:40 +0330

    added text input and fixed '/' key bug

--- scripts/script.js
+++ scripts/script.js
@@ -119,7 +119,7 @@
 //   } 
 // });
 
-// Map keys to their corresponding button IDs and highlight classes
+// Map keys to their corresponding button IDs and classes using nested objects
 const keyMap = {
   "0": { id: "btn0", class: "nes-btn-primary-keyboard" },
   "1": { id: "btn1", class: "nes-btn-primary-keyboard" },
@@ -134,7 +134,7 @@ const keyMap = {
   "+": { id: "btnp", class: "nes-btn-warning-keyboard" },
   "-": { id: "btnm", class: "nes-btn-warning-keyboard" },
   "*": { id: "btnx", class: "nes-btn-warning-keyboard" },
-  "d": { id: "btnd", class: "nes-btn-warning-keyboard" },
+  "/": { id: "btnd", class: "nes-btn-warning-keyboard" },
   "c": { id: "btnc", class: "nes-btn-error-keyboard" },
   "=": { id: "btne", class: "nes-btn-success-keyboard" }
 };
@@ -149,12 +149,14 @@ document.addEventListener("keydown", function (event) {
   if (mapping && !event.repeat && !activeKeys.has(key)) {
     const btn = document.getElementById(mapping.id);
     if (btn) {
+      event.preventDefault(); // this is in the case of '/' key because the default behavior is to open quick search
       btn.click();
       btn.classList.add(mapping.class);
       activeKeys.add(key);
     }
   }
-});
+ }
+);
 
 document.addEventListener("keyup", function (event) {
   const key = event.key;
